60cd0e851824b4af4f2ddf61fe2428cc5fd1a975
BebeShen <55395848+BebeShen@users.noreply.github.com>
2021-03-30 09:55:31 +0800

    [Fix] fix typo

--- LABs/lab1/lab1_file_transfer.c
+++ LABs/lab1/lab1_file_transfer.c
@@ -159,12 +159,12 @@ void udpSendFile(int sockfd, struct sockaddr_in addr_con, int addrlen){
 void checkProcess(long recv_size, long file_size, int* _25){
     char timebuf[80];
     time_t cur_time;
-    if(recv_size >= (file_size/4)*(_25*)){
+    if(recv_size >= (file_size/4)*(*_25)){
         printf("[Info] Data received:%ld/%ld\n",recv_size,file_size);
         time(&cur_time);
         strftime(timebuf, 80, "%Y/%m/%d %X", localtime(&cur_time));
-        printf("[Info]:%d%% %s\n\n", (_25*)*25, timebuf);
-        (_25*)++;
+        printf("[Info]:%d%% %s\n\n", (*_25)*25, timebuf);
+        (*_25)++;
     }
 }
 // function to receive file
