61d9aa3dc7ef5e6e0093f776fb6df4de428e23d0
BebeShen <55395848+BebeShen@users.noreply.github.com>
2021-03-30 16:52:51 +0800

    [Fix] fix typo

--- LABs/lab1/lab1_file_transfer.c
+++ LABs/lab1/lab1_file_transfer.c
@@ -163,7 +163,7 @@ void udpSendFile(int sockfd, struct sockaddr_in addr_con, int addrlen){
         // send
         sendto(sockfd, buffer, buffer_SIZE, sendrecvflag, (struct sockaddr*)&addr_con, addrlen);
         if(send_size >= (file_size/4)*(_25)){
-            printf("[Info] Data received:%ld/%ld\n",send_size,file_size);
+            printf("[Info] Data send:%ld/%ld\n",send_size,file_size);
             time(&cur_time);
             strftime(timebuf, 80, "%Y/%m/%d %X", localtime(&cur_time));
             printf("[Info]:%d%% %s\n\n", (_25)*25, timebuf);
